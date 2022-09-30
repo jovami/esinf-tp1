5620453572fd993abb16f3fa2a8f8028e7c59304
1210951 <1210951@isep.ipp.pt>
2022-09-30 21:23:49 +0100

    added relations data structures between pais and fruto

---
+++ src/main/java/esinf/model/ProducaoAno.java
@@ -0,0 +1,14 @@
+package esinf.model;
+
+import java.time.LocalDate;
+import java.util.HashMap;
+
+
+public class ProducaoAno {
+    
+    private HashMap <Integer, ProducaoFrutoPorPaisPorAno> prodAnual;
+
+    private LocalDate Ano;
+
+    
+}
---
+++ src/main/java/esinf/model/ProducaoFrutoPorPaisPorAno.java
@@ -0,0 +1,8 @@
+package esinf.model;
+
+//TODO: mudar o nome desta classe
+public class ProducaoFrutoPorPaisPorAno {
+    private Fruto fruto;
+    private int quantidadeProducao;
+
+}
