7f5e32e913a31d12208f7f08fad876d075028798
ruben_f_ferreira <1210954@isep.ipp.pt>
2022-10-01 19:25:10 +0100

    fix(junit): switch to junit jupiter

--- src/test/java/esinf/MainTest.java
+++ src/test/java/esinf/MainTest.java
@@ -1,36 +1,16 @@
 package esinf;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
 
 /**
  * Unit test for simple App.
  */
-public class MainTest
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public MainTest( String testName )
-    {
-        super( testName );
-    }
-
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( MainTest.class );
-    }
-
+public class MainTest {
     /**
      * Rigourous Test :-)
      */
+    @Test
     public void testApp()
     {
         assertTrue( true );
