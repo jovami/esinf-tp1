037bb88e53cd070b6dc170df5f847ed3691ae6ed
ruben_f_ferreira <1210954@isep.ipp.pt>
2022-10-01 01:00:30 +0100

    feat(pais): metodos para operar na LinkedList

--- src/main/java/esinf/model/Pais.java
+++ src/main/java/esinf/model/Pais.java
@@ -1,34 +1,72 @@
 package esinf.model;
 
+import java.util.Collection;
 import java.util.LinkedList;
+import java.util.List;
+import java.util.ListIterator;
 
 public class Pais {
 
-    //TODO: methods to operate this Linked List
     private LinkedList<ProducaoAno> producaoAnual;
 
     private String nomePais;
     private int paisCodigo;
 
-    public Pais(String nomePais, int paisCodigo){
+    public Pais(String nomePais, int paisCodigo) {
+        this.producaoAnual = new LinkedList<>();
+
         setNomePais(nomePais);
-        setoCodigoPais(paisCodigo);
+        setCodigoPais(paisCodigo);
+    }
+
+    public Pais(String nome, int codigo, Collection<ProducaoAno> producoes) {
+        this(nome, codigo);
+        this.producaoAnual.addAll(producoes);
+    }
+
+    public Pais(String nome, int codigo, ProducaoAno... producoes) {
+        this(nome, codigo, List.of(producoes));
+    }
+
+    private void setNomePais(String nome) {
+        if (nome == null || nome.trim().isEmpty())
+            throw new IllegalArgumentException("erro: nome invalido para o pais");
+        this.nomePais = nome;
+    }
+
+    private void setCodigoPais(int paisCodigo) {
+        this.paisCodigo = paisCodigo;
     }
 
     public String getNomePais() {
         return nomePais;
     }
 
-    public void setNomePais(String paisNome) {
-        this.nomePais = paisNome;
-    }
-
     public int getPaisCodigo() {
         return paisCodigo;
     }
 
-    public void setoCodigoPais(int paisCodigo) {
-        this.paisCodigo = paisCodigo;
+    // LinkedList
+
+    public boolean addAnoProducao(ProducaoAno ano) {
+        if (ano == null)
+            throw new IllegalArgumentException("erro: ano invalido");
+
+        return this.producaoAnual.add(ano);
     }
-    
+
+    public boolean addAllAnos(Collection<ProducaoAno> producoes) {
+        if (producoes == null || producoes.isEmpty())
+            throw new IllegalArgumentException("erro: anos invalidos");
+
+        return this.producaoAnual.addAll(producoes);
+    }
+
+    public ListIterator<ProducaoAno> getIteradorAnos() {
+        return this.producaoAnual.listIterator();
+    }
+
+    public ListIterator<ProducaoAno> getIteradorAnos(int indice) {
+        return this.producaoAnual.listIterator(indice);
+    }
 }
