e91b94bd23827e3e27fcf05b5a889203fa80103e
ruben_f_ferreira <1210954@isep.ipp.pt>
2022-10-01 01:30:15 +0100

    feat(paisStore): metodos para trabalhar com PaisStore

--- src/main/java/esinf/model/store/PaisStore.java
+++ src/main/java/esinf/model/store/PaisStore.java
@@ -1,30 +1,43 @@
 package esinf.model.store;
 
-import java.util.HashMap;
-
 import esinf.model.Pais;
 
+import java.util.HashMap;
+import java.util.Iterator;
+
 public class PaisStore {
+
     private HashMap<Integer,Pais> paisStore;
 
-    public PaisStore(){
-        paisStore = new HashMap<>();
+    public PaisStore() {
+        // num paises no mundo; evita re-hashing
+        // Taiwan + https://www.worldometers.info/geography/how-many-countries-are-there-in-the-world/
+        final int numCountries = 196;
+        this.paisStore = new HashMap<>(numCountries);
     }
-    
+
+    public int getSize() {
+        return this.paisStore.size();
+    }
+
     //TODO: necessário?
     public boolean checkPais(int id){
-        return paisStore.containsKey(id);
+        return this.paisStore.containsKey(id);
     }
-    
-    //TODO: addPais
-    public boolean addPais(int id,String paisNome){
 
-        if((paisStore.put(id,new Pais(paisNome,id)))!=null){
-            return true;
-        }
+    public boolean addPais(int id, String paisNome) {
+        if (this.paisStore.containsKey(id))
+            return false; // throw excep instead:
 
-        return false;
-        
+        return this.paisStore.put(id, new Pais(paisNome,id)) != null;
     }
-    
+
+    public Pais getPais(int id) {
+        // TODO: nao returnar ref?
+        return this.paisStore.get(id);
+    }
+
+    public Iterator<Pais> getIteradorPais() {
+        return this.paisStore.values().iterator();
+    }
 }
