939b10b6564d9b001d956f96d444eafa2312bc25
ruben_f_ferreira <1210954@isep.ipp.pt>
2022-10-01 12:34:23 +0100

    feat(app): Classe estilo singleton para instanciar PaisStore

--- src/main/java/esinf/App.java
+++ src/main/java/esinf/App.java
@@ -1,13 +1,31 @@
 package esinf;
 
+import esinf.model.store.PaisStore;
+
 /**
- * Hello world!
- *
+ * App
+ * Singleton pattern based class,
+ * used to obtain the PaisStore
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    private PaisStore store;
+
+    private App() {
+        this.store = new PaisStore();
+    }
+
+    public PaisStore getPaisStore() {
+        return this.store;
+    }
+
+    /* singleton pattern */
+    private static App singleton = null;
+    public static App getInstance() {
+        if (singleton == null) {
+            synchronized(App.class) {
+                singleton = new App();
+            }
+        }
+        return singleton;
     }
 }
