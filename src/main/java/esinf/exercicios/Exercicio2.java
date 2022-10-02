c5cbb81c7c28bcd701b9a1221f55c681e6853ffc
ruben_f_ferreira <1210954@isep.ipp.pt>
2022-10-02 14:11:40 +0100

    refactor(exercicio2): Remove Collections import
    Collections.sort() calls List's sort() method, so no need of importing
    Collections

--- src/main/java/esinf/exercicios/Exercicio2.java
+++ src/main/java/esinf/exercicios/Exercicio2.java
@@ -1,6 +1,5 @@
 package esinf.exercicios;
 
-import java.util.Collections;
 import java.util.Comparator;
 import java.util.Iterator;
 import java.util.LinkedList;
@@ -81,19 +80,14 @@ public class Exercicio2 implements Runnable {
         return triplets;
     }
 
-    /* T type alias */
-    public <T extends Triplet<Pais, Integer, Integer>> List<Pais>
-        sortPais(List<T> list, Comparator<T> cmp)
+    /* E type alias */
+    public <E extends Triplet<Pais, Integer, Integer>> List<Pais>
+        sortPais(List<E> list, Comparator<E> cmp)
     {
-        // return list.stream()
-        //            .sorted(cmp)
-        //            .map(t -> t.getFirst())
-        //            .toList();
-
         var result = new LinkedList<Pais>();
-        Collections.sort(list, cmp);
 
-        list.forEach(t -> result.add(t.getFirst()));
+        list.sort(cmp);
+        list.forEach(e -> result.add(e.getFirst()));
         return result;
     }
 }
