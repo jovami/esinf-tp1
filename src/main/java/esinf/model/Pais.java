c29b55b5814e36f52695faeb8ab5a3f23f05008e
unknown <1210951@isep.ipp.pt>
2022-10-06 13:14:37 +0100

    fixed:classe pais agr é responsável por adicionar dados sobre os frutos e producoes anuais dos mesmos

--- src/main/java/esinf/model/Pais.java
+++ src/main/java/esinf/model/Pais.java
@@ -67,6 +67,13 @@ public class Pais implements Iterable<ProducaoAno> {
         return ok;
     }
 
+    //pedir o inteiro ou o anoProducao?
+    public void addProducaoFruto(int ano,Fruto fruto,int quantidadeProd) {   
+        getProducaoAno(ano).addProducaoFruto(fruto, quantidadeProd);
+        incrementProducao(quantidadeProd);
+        //throw caso o ano não exista??
+    }
+
     public ProducaoAno createAnoProducao(int ano) {
         return new ProducaoAno(ano);
     }
