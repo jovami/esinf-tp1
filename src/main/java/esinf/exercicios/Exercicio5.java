72189e8a37f07f6dc957790ea1f0eec92637d924
jonasantunes <1181478@isep.ipp.pt>
2022-10-07 16:04:41 +0100

    feature(exercicio5) : completed

--- src/main/java/esinf/exercicios/Exercicio5.java
+++ src/main/java/esinf/exercicios/Exercicio5.java
@@ -6,6 +6,7 @@ import esinf.model.Pais;
 import esinf.model.ProducaoAno;
 import esinf.model.ProducaoFrutoPorPaisPorAno;
 import esinf.util.ListPrinter;
+import esinf.util.Pair;
 import esinf.util.Triplet;
 import java.util.*;
 
@@ -17,15 +18,6 @@ public class Exercicio5 implements Runnable {
 
     private App app;
 
-    //comparator para os nomes dos frutos
-    private Comparator<Triplet<Integer, Integer, Integer>> cmpIDFruto =
-            (t1 , t2) -> Integer.compare(t1.getSecond(), t2.getSecond());
-
-    //comparator para as quantidades de producao
-    private Comparator<Triplet<Integer, Integer, Integer>> cmpQuantProducao =
-            (t1 , t2) -> Integer.compare(t1.getThird(), t2.getThird());
-
-
     public Exercicio5() {
         app = App.getInstance();
     }
@@ -35,167 +27,71 @@ public class Exercicio5 implements Runnable {
     @Override
     public void run() {
 
-        // Colocar aqui codigo que queiram correr
-       /* app.getPaisStore().addPais(174,"Portugal");
-        app.getFrutoStore().addFruto(515,"Apples");
-        app.getFrutoStore().addFruto(486,"Bananas");
-
-
-        Pais pais = app.getPaisStore().getPais(174);
-        Fruto frutoApples = app.getFrutoStore().getFruto(515);
-        Fruto frutoBananas = app.getFrutoStore().getFruto(486);
-
-        pais.addAnoProducao(pais.createAnoProducao(2000));
-        ProducaoAno producaoAno = pais.getProducaoAno(2000);
-
-        pais.addAnoProducao(pais.createAnoProducao(2001));
-        ProducaoAno producaoAno1 = pais.getProducaoAno(2001);
-
-        pais.addAnoProducao(pais.createAnoProducao(2002));
-        ProducaoAno producaoAno2 = pais.getProducaoAno(2002);
-
-        producaoAno.addProducaoFruto(frutoApples,229794);
-        producaoAno.addProducaoFruto(frutoBananas,30518);
-
-        producaoAno1.addProducaoFruto(frutoApples,258363);
-        producaoAno1.addProducaoFruto(frutoBananas,28304);
-
-        producaoAno2.addProducaoFruto(frutoApples,300225);
-        producaoAno2.addProducaoFruto(frutoBananas,29227);
-*/
-
-        /*
-        System.out.println("CodePais(id) "+pais.getPaisCodigo()+ " PaisNome "+pais.getNomePais());
-
-        System.out.println("Frutonome=" +pais.getProducaoAno(2000).getProducaoFruto(515).getFruto().getNome()+
-                " Ano=" + pais.getProducaoAno(2000).getAno()+" Quantidadeproducao=" + pais.getProducaoAno(2000).getProducaoFruto(515).getQuantidadeProducao());
-
-        System.out.println("Frutonome=" +pais.getProducaoAno(2001).getProducaoFruto(515).getFruto().getNome()+
-                " Ano=" + pais.getProducaoAno(2001).getAno()+" Quantidadeproducao=" + pais.getProducaoAno(2001).getProducaoFruto(515).getQuantidadeProducao());
-
-        System.out.println("Frutonome=" +pais.getProducaoAno(2002).getProducaoFruto(515).getFruto().getNome()+
-                " Ano=" + pais.getProducaoAno(2002).getAno()+" Quantidadeproducao=" + pais.getProducaoAno(2002).getProducaoFruto(515).getQuantidadeProducao());
-
-        System.out.println("Frutonome=" +pais.getProducaoAno(2000).getProducaoFruto(486).getFruto().getNome()+
-                " Ano=" + pais.getProducaoAno(2000).getAno()+" Quantidadeproducao=" + pais.getProducaoAno(2000).getProducaoFruto(486).getQuantidadeProducao());
-
-        System.out.println("Frutonome=" +pais.getProducaoAno(2001).getProducaoFruto(486).getFruto().getNome()+
-                " Ano=" + pais.getProducaoAno(2001).getAno()+" Quantidadeproducao=" + pais.getProducaoAno(2001).getProducaoFruto(486).getQuantidadeProducao());
-
-        System.out.println("Frutonome=" +pais.getProducaoAno(2002).getProducaoFruto(486).getFruto().getNome()+
-                " Ano=" + pais.getProducaoAno(2002).getAno()+" Quantidadeproducao=" + pais.getProducaoAno(2002).getProducaoFruto(486).getQuantidadeProducao());
-
-        */
-
-        //lista com todos os tripletos de [ano , frutoID , quantidadeproducao]
-        List tripletos = getListProdFrutosQuantidadeAno(174);
-
-        //Ordenar lista por frutoID seguido de quantidadeproducao
-        List tripletosOrdenados = sortPorFrutosIDPorQuantidadeProducaoAno(tripletos,cmpIDFruto,cmpQuantProducao);
-
-        //Fazer a diferenca dos absolutos
-        List<String> estrutura = getEstruturaPedida(tripletosOrdenados);
-
-        //Imprimir a lista pedida
-        ListPrinter.print(estrutura,"[AnoDaProducaoMinima/AnoDaProducaoMaxima,Nome do Fruto(ordenado por IDFruto), Diff Abs das quantidades de producao]\n",null);
+        String estruta = getEstruturaPedida(174);
+        System.out.println(estruta);
 
     }
 
 
-
-    public List<Triplet<Integer, Integer, Integer>> getListProdFrutosQuantidadeAno(int idPais)
+    public String getEstruturaPedida(Integer idPais)
     {
 
-        // [ano , frutoID , quantidadeproducao]
-        var estrutura = new LinkedList<Triplet<Integer, Integer, Integer>>();
-        Pais pais =  app.getPaisStore().getPais(idPais);
+        int i = idPais;
 
-        for (Iterator<ProducaoAno> producaoAnualIter = pais.iterator(); producaoAnualIter.hasNext(); )
-        {
-            if (!producaoAnualIter.hasNext())
-                throw new RuntimeException("erro: nao ha producao naquele ano");
+        final int maxFrutos = app.getFrutoStore().size();
+        HashMap<Fruto, Pair<ProducaoAno, Integer>> minProducao = new HashMap<>(maxFrutos);
+        HashMap<Fruto, Pair<ProducaoAno, Integer>> maxProducao = new HashMap<>(maxFrutos);
 
-            ProducaoAno prodAnual = producaoAnualIter.next();
+        app.getPaisStore().getPais(i).forEach(ano -> {
+            ano.forEach(prod -> {
+                Fruto f = prod.getFruto();
+                int qtd = prod.getQuantidadeProducao();
 
-            //Iterator<ProducaoFrutoPorPaisPorAno>
-            for(Iterator<ProducaoFrutoPorPaisPorAno> producaoFrutoPorPaisPorAnoIter =  prodAnual.iterator(); producaoFrutoPorPaisPorAnoIter.hasNext(); )
-            {
-                if (!producaoFrutoPorPaisPorAnoIter.hasNext())
-                    throw new RuntimeException("erro: nao ha producao para aquele fruto naquele ano");
+                if (minProducao.get(f) == null
+                        || minProducao.get(f).getSecond() > qtd)
+                    minProducao.put(f, new Pair<ProducaoAno,Integer>(ano, qtd));
 
-                ProducaoFrutoPorPaisPorAno prodFrutoPorPaisPorAno = producaoFrutoPorPaisPorAnoIter.next();
-
-                // [ano , frutoID , quantidadeproducao]
-                estrutura.add(new Triplet<Integer, Integer, Integer>(prodAnual.getAno(), prodFrutoPorPaisPorAno.getFruto().getId(), prodFrutoPorPaisPorAno.getQuantidadeProducao()));
-
-            }
-
-        }
-
-       return estrutura;
-    }
-
-
-    public <E extends Triplet<Integer, Integer, Integer>> List<Triplet<Integer, Integer, Integer>> sortPorFrutosIDPorQuantidadeProducaoAno(List<E> list, Comparator<E> cmpIDFruto, Comparator<E> cmpQuantProducao)
-    {
-        var result = new LinkedList<Triplet<Integer, Integer, Integer>>();
-
-        list.sort(cmpIDFruto.thenComparing(cmpQuantProducao));
-        list.forEach(e -> result.add(new Triplet<Integer, Integer, Integer>(e.getFirst(),e.getSecond(),e.getThird())));
-
-        return result;
-    }
-
-
-    public <E extends Triplet<Integer, Integer, Integer>> List<String> getEstruturaPedida(List<E> list)
-    {
-
-        var result = new LinkedList<String>();
-        final int[] start = {list.get(0).getSecond()};
-        final int[] max = {list.get(0).getThird()};
-        final int[] yearMax = {list.get(0).getFirst()};
-        final int[] min = {list.get(0).getThird()};
-        final int[] yearMin = {list.get(0).getFirst()};
-
-        // TODO: VER SE NAO HA UMA MELHOR MANEIRA DE FAZER ESTA PARTE, TALVEZ COM UM ITERADOR DENTRO DO FOREACH
-        list.forEach(e -> {
-            if(e.getSecond() == start[0])
-            {
-                if(e.getThird() >= max[0])
-                {
-                    max[0] = e.getThird();
-                    yearMax[0] = e.getFirst().intValue();
-                }
-                else if (e.getThird().intValue() < min[0])
-                {
-                    min[0] = e.getThird().intValue();
-                    yearMin[0] = e.getFirst().intValue();
-                }
-            }
-            else
-            {
-
-                int diff = Math.abs(max[0] - min[0]);
-                String s = "["+yearMin[0]+"/"+yearMax[0]+ "," + app.getFrutoStore().getFruto(start[0]).getNome() + "," + diff + "]";
-                result.add(s);
-
-                start[0] = e.getSecond().intValue();
-                max[0] = e.getThird().intValue();
-                min[0]= e.getThird().intValue();
-                yearMin[0] = e.getFirst().intValue();
-                yearMax[0] = e.getFirst().intValue();
-
-            }
-
+                if (maxProducao.get(f) == null
+                        || maxProducao.get(f).getSecond() < qtd)
+                    maxProducao.put(f, new Pair<ProducaoAno,Integer>(ano, qtd));
+            });
         });
 
-        int diff = Math.abs(max[0] - min[0]);
+        Iterator<Fruto> iter = app.getFrutoStore().iterator();
 
-        // [anox/anoy , nomefruto , diferenca entre o maior e menor valor de producao]
-        String s = "["+yearMin[0]+"/"+yearMax[0]+ "," + app.getFrutoStore().getFruto(start[0]).getNome() + "," + diff + "]";
-        result.add(s);
+        if (!iter.hasNext())
+            throw new RuntimeException("this shouldn't even be possible");
 
-        return result;
+        Fruto f1 = iter.next();
+
+        int maxSoFar = maxProducao.get(f1).getSecond() - minProducao.get(f1).getSecond();
+        Fruto frutoKey = f1;
+
+        while (iter.hasNext()) {
+            Fruto f = iter.next();
+
+            if(!maxProducao.containsKey(f) ||  !minProducao.containsKey(f))
+                continue;
+
+
+            int diff = maxProducao.get(f).getSecond() - minProducao.get(f).getSecond();
+
+
+            if (diff > maxSoFar) {
+                maxSoFar = diff;
+                frutoKey = f;
+            }
+        }
+
+        Pair<Integer, Integer> anoMinMax = new Pair<>(minProducao.get(frutoKey).getFirst().getAno(), maxProducao.get(frutoKey).getFirst().getAno());
+
+        Triplet<Pair<Integer, Integer>, Fruto, Integer> resposta = new Triplet<>(anoMinMax, frutoKey, maxSoFar);
+
+
+        String estrutura = "["+resposta.getFirst().getFirst()+"/"+resposta.getFirst().getSecond()+ "," + resposta.getSecond().getNome() + "," + resposta.getThird() + "]";
+
+        return estrutura;
+
     }
 
 }
